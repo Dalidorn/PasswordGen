811c7447dd1b677c30fc8997193228e0403651fc
Nate <fritz.nathaniel97@gmail.com>
2022-10-26 23:12:12 -0400

    refactors js code, adds comment explainations

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,81 +7,58 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Add event listener to generate buttoninput
 generateBtn.addEventListener("click", writePassword);
 
-
-
-// My stuff
+// ---------------My stuff-----------------
 
 function generatePassword() {
 
-  var input = prompt("How long would you like the password to be? (8-128 characters)");
-  var allowed = {
-    alpha: ["abcdefghijklmnopqrstuvwxyz"],
-    numeric: ["0123456789"],
-    special: ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
-    amount: 0,
-    accepted: ""
-  };
-  var truths = 0;
+  // declaring local variables.
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var numeric = "0123456789";
+  var special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  var amount = prompt("How long would you like the password to be? (8-128 characters)");
+  var useable = "";
   var result = "";
 
-  if(input == null) {
-    return "Changed your mind?";
-  }
+  // function to call later - adds variable content to useable variable and one of variable's characters to result
+  function rando(variable) {
+    useable += variable;
+    result += variable.charAt(Math.floor(Math.random() * variable.length));
+  };
 
-  if(input < 8 || input > 128 || !parseInt(input, 10)) {
+  // if the first prompt is canceled, prompt will return 'null'.
+  if(amount == null) {
+    return "Changed your mind?";
+  };
+
+  // testing if amount is within parameters, and using !parseInt to catch if it's not a number.
+  if(amount < 8 || amount > 128 || !parseInt(amount, 10)) {
     alert("Please enter only a number between 8 and 128");
     return generatePassword();
-  } 
+  };
   
-  else {
-    allowed.amount = input;
-    allowed.alpha.push(confirm("Including lower case letters?"))
-    allowed.alpha.push(confirm("Including capital letters?"))
-    allowed.numeric.push(confirm("Including numbers?"))
-    allowed.special.push(confirm("Including special characters?"))
-  };
+  // prompts for each option, if accepted calls rando function.
+  if(confirm("Including lower case letters?")) rando(alpha);
 
-  if(allowed.alpha[1]) {
-    allowed.accepted += allowed.alpha[0];
-    truths++
-    result += allowed.alpha[0].charAt(Math.floor(Math.random() * allowed.alpha[0].length));
-  };
+  if(confirm("Including capital letters?")) rando(alpha.toUpperCase());
 
+  if(confirm("Including numbers?")) rando(numeric);
 
-  if(allowed.alpha[2]) {
-    allowed.accepted += allowed.alpha[0].toUpperCase();
-    truths++
-    result += allowed.alpha[0].charAt(Math.floor(Math.random() * allowed.alpha[0].length)).toUpperCase();
-  };
+  if(confirm("Including special characters?")) rando(special);
 
-
-  if(allowed.numeric[1]) {
-    allowed.accepted += allowed.numeric[0];
-    truths++
-    result += allowed.numeric[0].charAt(Math.floor(Math.random() * allowed.numeric[0].length));
-  };
-
-
-  if(allowed.special[1]) {
-    allowed.accepted += allowed.special[0];
-    truths++
-    result += allowed.special[0].charAt(Math.floor(Math.random() * allowed.special[0].length));
-  };
-
-
-  if(truths < 1) {
+  // ensures one prompt was confirmed.
+  if(result.length < 1) {
     alert("Must allow at least one option! Please try again.");
     return generatePassword();
   };
 
-  for(i = 1; i <= allowed.amount - truths; i++) {
-    result += allowed.accepted.charAt(Math.floor(Math.random() * allowed.accepted.length));
+  // interation loops to choose random char within the useable variable.
+  while(amount > result.length) {
+    result += useable.charAt(Math.floor(Math.random() * useable.length));
   };
 
   return result;
